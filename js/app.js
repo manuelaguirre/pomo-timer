ed4b6a70d6ac6495a3348a0653ca8d8ea604cd02
Manuel Aguirre <aguirre.manuel7@gmail.com>
2019-12-31 16:57:04 +0100

    Clock logic improved

--- js/app.js
+++ js/app.js
@@ -14,17 +14,45 @@ function renderClock(time) {
     $clockDisplay.innerText = formatAsTimer(time);
 }
 
-function runClock(initialSeconds) {
+function runClock() {
+    let $clockDisplay = document.querySelector("#clock-display");
+    let seconds = formatTimerAsSeconds($clockDisplay.innerText);
     intervalID = setInterval(() => {
-        renderClock(initialSeconds--);
-        
+        renderClock(--seconds);
     }, 1000);
 }
-
-
 
 function formatAsTimer(totalSeconds) {
     let minutes = (Math.floor(totalSeconds / 60)).toString().padStart(2, '0');
     let seconds = (totalSeconds % 60).toString().padStart(2, '0');
-    return `${minutes}:${seconds}`
+    return `${minutes}:${seconds}`;
+}
+
+function formatTimerAsSeconds($timer) {
+    let minutes = parseInt($timer[0] + $timer[1], 10);
+    let seconds = parseInt($timer[3] + $timer[4], 10);
+    seconds = minutes * 60 + seconds;
+    return seconds;
+}
+
+function stopClock() {
+    //I want it to return the stopping time in total seconds
+    clearInterval(intervalID);
+    let $clockDisplay = document.querySelector("#clock-display");
+    return formatTimerAsSeconds($clockDisplay.innerText);
+}
+
+function setTimeBlock(seconds) {
+    let $clockDisplay = document.querySelector("#clock-display");
+    $clockDisplay.innerText = formatAsTimer(seconds);
+}
+
+function name(params) {
+    
+}
+
+
+
+function name(params) {
+
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const $clockDisplay = document.querySelector("#clock-display");
+
 
 
 
